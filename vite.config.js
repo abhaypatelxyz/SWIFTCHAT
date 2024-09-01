01299fd1fa6fefe6c8fe802b456bf3033ebfa6eb
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-09-01 19:38:41 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://chat-box-server-nine.vercel.app',
+        changeOrigin: true,
+        secure: false,
+        ws: true, // Enable WebSocket proxying if using WebSockets
+      },
+    },
+  },
+});
