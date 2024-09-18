8a8c7c2d92148309ecb61b4f2b440ef6f1ba869c
SHUBHAM DAS <149561473+ShubhamDaS-IIT-Dhanbad@users.noreply.github.com>
2024-09-19 02:19:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React, { lazy, Suspense, useEffect, useState } from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import './App.css';
 import { useSocket } from './socketProvider.jsx'; // Access the socket through SocketProvider
-import axios from 'axios'
+import axios from 'axios';
+
+// Lazy loading components
 const Home = lazy(() => import('./component/home.jsx'));
 const Login = lazy(() => import('./component/login.jsx'));
 
@@ -20,41 +22,45 @@ const App = () => {
       const parsedUser = JSON.parse(localStorageUser);
       setUser(parsedUser);
       console.log(parsedUser);
-      
 
       const updateSocketId = async () => {
         try {
-            await axios.post(`${BASE_URL}/api/updatedata`, {
-                uid: user.uid,
-                socketId: socket.id
-            });
-            console.log('Socket ID updated successfully', socket.id);
+          await axios.post(`${BASE_URL}/api/updatedata`, {
+            uid: parsedUser.uid,
+            socketId: socket.id
+          });
+          console.log('Socket ID updated successfully', socket.id);
         } catch (error) {
-            console.error('Error updating socket ID:', error);
+          console.error('Error updating socket ID:', error);
         }
-    };
-    updateSocketId();
-
+      };
+      updateSocketId();
 
       navigate('/');
     } else {
       navigate('/login');
     }
-  }, [navigate]);
+  }, [navigate, socket.id]);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        {user ? (
-          <Route
-            path='/'
-            element={<Home user={user} setUser={setUser} socket={socket} contact={contact} setContact={setContact} />}
-          />
-        ) : (
-          <>
-            <Route path='/login' element={<Login setUser={setUser} socket={socket} />} />
-          </>
-        )}
+        <Route
+          path='/'
+          element={user ? (
+            <Home user={user} setUser={setUser} socket={socket} contact={contact} setContact={setContact} />
+          ) : (
+            <Navigate to='/login' />
+          )}
+        />
+        <Route
+          path='/login'
+          element={!user ? (
+            <Login setUser={setUser} socket={socket} />
+          ) : (
+            <Navigate to='/' />
+          )}
+        />
         <Route path='*' element={<Navigate to={user ? '/' : '/login'} />} />
       </Routes>
     </Suspense>
